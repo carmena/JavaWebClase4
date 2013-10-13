551f4a23284717befa22c2b274902faee78f9ffc
LAB704-00 <LAB704-00@LAB704-02>
2013-10-13 11:24:05 -0500

    proyecto web

--- src/main/java/app/model/Campo.java
+++ src/main/java/app/model/Campo.java
@@ -9,5 +9,59 @@ package app.model;
  * @author LAB704-00
  */
 public class Campo {
-    
+
+    private int id;
+    private String descripcion;
+    private int estado;
+    private int tipo;
+    private double osto_hora;
+    private int id_local;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getOsto_hora() {
+        return osto_hora;
+    }
+
+    public void setOsto_hora(double osto_hora) {
+        this.osto_hora = osto_hora;
+    }
+
+    public int getId_local() {
+        return id_local;
+    }
+
+    public void setId_local(int id_local) {
+        this.id_local = id_local;
+    }
 }
